bbc39ee0fbefb4aeb7e69e880857f7604649cb9b
doudou459 <459349489@qq.com>
2019-11-19 17:32:52 +0800

    2019-11-19

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import ajax from 'axios'
+import md5 from 'js-md5'
+Vue.config.productionTip = false
+Vue.use(element)
+Vue.prototype.$md5 = md5
+Vue.prototype.$ajax = ajax
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="loginWeb">
+    <el-form :model="user" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="账号" prop="loginID">
+        <el-input v-model="user.loginID" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="loginKey">
+        <el-input type="password" v-model="user.loginKey" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('user')">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data: function() {
+    return {
+      user: {
+        loginID: "",
+        loginKey: ""
+      }
+    }
+  }
+}
+</script>
