b6ec046d5183193550681a025a328c8b0afb5225
doudou459 <459349489@qq.com>
2020-01-08 17:35:58 +0800

    2020-1-8

---
+++ src/components/wangeditor.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+      <div id="editorTool" class="editorTool"></div>
+      <div id="editor" class="editor"></div>
+    </div>
+    
+</template>
+
+<script>
+const Editor = require('wangeditor');
+export default {
+    mounted(){
+     let editor = new Editor('#editorTool','#editor');
+      editor.customConfig.menus=[ 
+            'head',  // 标题
+            'bold',  // 粗体
+            'fontSize',  // 字号
+            'fontName',  // 字体
+            'underline',  // 下划线
+            'foreColor',  // 文字颜色
+            'backColor',  // 背景颜色
+            'link',  // 插入链接
+             'list',  // 列表
+            'justify',  // 对齐方式
+            'image',  // 插入图片
+            'table',  // 表格
+            'undo',  // 撤销
+            'redo'  // 重复
+            ]     
+     editor.create();
+
+    }
+}
+</script>
+
+<style scoped>
+.editorTool{
+    padding-top: 5px;
+    padding-bottom:5px;
+    border-bottom-style:inset;
+    border-bottom-width:1px;
+    border-bottom-color: azure;
+}
+.editor{
+    text-align: left;
+}
+</style>
--- src/views/setProduce.vue
+++ src/views/setProduce.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>setProduce</div>
+    <div>
+        <editor></editor>
+    </div>
 </template>
+<script>
+import editor from "../components/wangeditor"
+export default {
+      components: {
+    editor
+  }
+}
+</script>
